6d4578b15b6235ad98ceac91f3f9293a20372a79
James Devine <fxmulder@gmail.com>
2023-01-29 16:17:09 -0600

    amd64 init

--- repo/ubuntu-update.sh
+++ repo/ubuntu-update.sh
@@ -6,6 +6,9 @@ cd ubuntu/
 dpkg-scanpackages --arch all pool/ > dists/stable/main/binary-arm64/Packages
 cat dists/stable/main/binary-arm64/Packages | gzip -9 > dists/stable/main/binary-arm64/Packages.gz
 
+dpkg-scanpackages --arch all pool/ > dists/stable/main/binary-amd64/Packages
+cat dists/stable/main/binary-amd64/Packages | gzip -9 > dists/stable/main/binary-amd64/Packages.gz
+
 cd dists/stable
 
 # release
